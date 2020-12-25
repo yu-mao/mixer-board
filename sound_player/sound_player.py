eb4f14632418817e5e5629f7c42fa5fb3f429ca2
Jiayao Yu <jiayao.yu@ericsson.com>
2020-12-25 19:01:51 +0100

    Jiayao added code to read sound files

--- sound_player/sound_player.py
+++ sound_player/sound_player.py
@@ -1,6 +1,15 @@
+#!/usr/bin/env python3
+
 # TODOs
 
 # 1. find the sound files
 # 2. recognise button input to play the right sound files
 # 3. play the sound files
+
+from playsound import playsound
+
+path_to_sound_files = "/sound_files/"
+# open sound files by specified sound files path
+for sound_file in sound_files:
+	playsound(sound_file)
 
