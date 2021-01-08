a44eb00d8d4a7409b60c7b03cfe774dd57f8ce62
Jiayao Yu <jiayao.yu@ericsson.com>
2021-01-08 22:43:56 +0100

    Updated sound playing python code.

--- sound_player/sound_player.py
+++ sound_player/sound_player.py
@@ -3,7 +3,7 @@
 import os
 
 import serial
-import playsound
+from playsound import playsound
 
 """
 Args:
@@ -19,27 +19,28 @@ class SoundPlayer():
 	def __init__(self, com_port_number='COM5', 
 					   baud_rate=115200, 
 					   timeout=0.1,
-					   sound_files_path='/sound_files/'):
+					   sound_files_path='./sound_files/'):
 		self.arduino = serial.Serial(com_port_number, baud_rate, timeout=timeout)
-		self.buttonPressed = 0
+		self.button_pressed  = 0
 		self.sound_files_path = sound_files_path
 
+# TODO: detect button press CHANGE
 	def read_arduino(self):
 		while True:
 			data_from_arduino = self.arduino.readline()[:-2]
 			if data_from_arduino:
-				print(data_from_arduino)
-				self.button_pressed = (data_from_arduino[-1] + 2) % 10
-				print(self.button_pressed)
-				self.play()
+				button_pressed = (data_from_arduino[-1] + 2) % 10
+				if button_pressed != self.button_pressed:
+					self.button_pressed = button_pressed
+					print(self.button_pressed)
+					self.play()
 
 	def play(self):
-		for root, dirs, files in os.walk(self.sound_files_path):
-			if str(self.button_pressed) in files:
-				sound_file_path = os.path.join(root, str(self.button_pressed))
-				print("////////////////////")
-				print(sound_file_path)
-				playsound(sound_file_path)
+		sound_file_names = os.listdir(self.sound_files_path)
+		for sound_file_name in sound_file_names:
+			if str(self.button_pressed) in sound_file_name:
+				sound_files_path = self.sound_files_path + sound_file_name
+				playsound(sound_files_path)
 
 
 if __name__ == "__main__":
