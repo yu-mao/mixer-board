bbd3519849457ec3c7b8533dafb5d58a898db8b7
calle <calle.trybom@gmail.com>
2021-03-02 19:38:25 +0100

    Fixed file Calletest, looking a bit prettier

--- sound_player/Calletest.py
+++ sound_player/Calletest.py
@@ -4,6 +4,9 @@ import serial
 import sys
 from playsound import playsound
 class SoundPlayer():
+
+#Initializing setup
+
 	def __init__(self, com_port_number='COM5', 
 					   baud_rate=115200, 
 					   timeout=0.1,
@@ -12,20 +15,12 @@ class SoundPlayer():
 		self.button_pressed  = 0
 		self.sound_files_path = sound_files_path
 
-
-#This fn can turn the printed "Button 1" etc. to the integer 1.
-	#def getNumbers(str): 
-    #			array = re.findall(r'[0-9]+', str) 
-    #			return array 
-
-#Transform output from arduino board to input into readable 
-#Python expressions
+#Reading arduino output
 
 	def reader(self):
 			while True:
 				output_arduino = self.arduino.readline()[:-2]
 				if output_arduino:
-					#nummer =getNumbers(output_arduino)
 					button_pressed = output_arduino[-1]
 					if button_pressed != self.button_pressed:
 						self.button_pressed = button_pressed
@@ -33,22 +28,22 @@ class SoundPlayer():
 						self.player()
 					
 
-#Reads which button is pressed and makes the computer and plays the corresponding file
+#Reads which button is pressed and plays the corresponding sound file
 
 	def player(self):
 			if self.button_pressed == 49:
 				playsound('./sound_files/1.wav')
-#C:\Users\calle\OneDrive\Skrivbord\Mixer-board\sound_player
+
 			elif self.button_pressed == 50:
 				playsound('./sound_files/2.wav')
-#C:\Users\calle\OneDrive\Skrivbord\Mixer-board\sound_player
+
 			elif self.button_pressed == 51:
 				playsound('./sound_files/3.wav')
-#C:\Users\calle\OneDrive\Skrivbord\Mixer-board\sound_player
+
 			elif self.button_pressed == 52:
 				playsound('./sound_files/4.wav')
-#C:\Users\calle\OneDrive\Skrivbord\Mixer-board\sound_player
 
+#This part makes the program run in cmd
 if __name__ == "__main__":
 	soundPlayer = SoundPlayer()
 	soundPlayer.reader()
